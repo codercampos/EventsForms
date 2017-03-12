a5fde9eb423a60e473291424a16d3d7f494e904b
Carlos Campos <ccampos@xinobistudio.com>
2017-03-12 17:20:12 -0600

    added models for Home feed information

---
+++ Events/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Events.Models
+{
+	public class Event
+	{
+		public string CoverImage { get; set; }
+		public DateTime EventDate { get; set; }
+		public string Title { get; set; }
+		public string Venue { get; set; }
+		public string City { get; set; }
+		public int UsersInterested { get; set; }
+
+		//Custom properties
+		public string FullVenue
+		{
+			get
+			{
+				return $"{Venue} • {City}";
+			}
+		}
+	}
+}
---
+++ Events/Models/EventFeed.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Events.Models
+{
+	public class EventFeed : IFeedMember
+	{
+		public long Id { get; set; }
+		public MemberType Type { get; set; }
+		public string UserName { get; set; }
+		public DateTime PublishDate { get; set; }
+		public PrivacyType Privacy { get; set; }
+		public Event Event { get; set; }
+
+		//Custom properties as helpers
+
+		/// <summary>
+		/// Gets the publish date with custom title depending when the user have accepted or shown interest on a event
+		/// </summary>
+		/// <value>The publish date with a custom title</value>
+		public string PublishDateString
+		{
+			get
+			{
+				var stringToPublish = string.Empty;
+				var daysPassed = DateTime.UtcNow - PublishDate.ToUniversalTime();
+				if (daysPassed.Days < 1)
+				{
+					return $"Today at {PublishDate.ToLocalTime().ToString("hh:mm tt")}";
+				}
+				else if (daysPassed.Days >= 1 && daysPassed.Days < 2)
+				{
+					return $"Yesterday at {PublishDate.ToLocalTime().ToString("hh:mm tt")}";
+				}
+				else
+				{
+					return PublishDate.ToLocalTime().ToString("MMM dd, yyyy");
+				}
+			}
+		}
+	}
+}
